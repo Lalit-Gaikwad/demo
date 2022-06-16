e8a59077bb743e1612b14cd31b207519aa90bea6
Lalit-Gaikwad <dekucypher251@gmail.com>
2022-06-16 10:19:25 +0530

    deploy

---
+++ chatApp/static/js/create.js
@@ -0,0 +1,26 @@
+const btnCreate = document.querySelector("#btn-create");
+
+
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+btnCreate.addEventListener('click', ()=>{
+   let roomName = '';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 12; i++ ) {
+        if(i==3 || i==7)
+            roomName += '-';
+        else
+            roomName += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(roomName);    
+    window.location.pathname = '/chat/'+ roomName + '/';
+
+})
+
+const btnJoin = document.querySelector('#btn-join');
+document.getElementById("btn-join").value = "";
+var key;
+btnJoin.addEventListener('click', ()=>{
+    key = document.getElementById('Room_id').value;
+    window.location.pathname = '/chat/'+ key + '/';
+})
